0e30771e95f9cc51ed429ce1ff9a900f564ae8d2
Satyadev Saini <105744852+SatyadevSaini@users.noreply.github.com>
2023-04-24 23:49:21 +0530

    my program of props passing data using prop
    hey here i am passing the data using props props are properties 
    prop : passing data into argument

---
+++ App.js
@@ -0,0 +1,12 @@
+//import logo from "./logo.svg";
+import "./App.css";
+import Expence from "./Component/ExpenceITEM";
+
+
+function App() {
+  return (
+<Expence /> 
+  );
+}
+
+export default App;
