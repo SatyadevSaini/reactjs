0b497d965e8f2ec33e6ba1e9b6092a16e1fee86d
Satyadev Saini <105744852+SatyadevSaini@users.noreply.github.com>
2023-04-26 21:56:39 +0530

    Date Addeds

---
+++ ExpenceITEM.js
@@ -0,0 +1,15 @@
+import "./Expence.css";
+
+function Expence() {
+  return (
+    <div className="mainClass">
+      <div className="expence-item">
+        <div> Monday 23 March 2023</div>
+        <div className="expence-item__description">Car Insurnce Works</div>
+        <div className="expence-item__price"> $ 234</div>
+      </div>
+    </div>
+  );
+}
+
+export default Expence;
